8e4e8d0eed320d625cb95bfdb61dfca5351f44e1
Om Patel <omunite21@gmail.com>
2023-06-08 17:24:43 +0530

    UI Fixes

--- sections/Recents.tsx
+++ sections/Recents.tsx
@@ -6,11 +6,12 @@ import { ProjectCard } from '@/components'
 import { AiFillGithub } from 'react-icons/ai'
 import styles from '@/styles';
 import { motion } from 'framer-motion';
-import { staggerContainer } from '@/utils/motion';
+import {slideIn} from '../utils/motion';
+
 
 const Recents = () => {
   return (
-    <motion.section className={`relative ${styles.flexBetween} md:flex-row flex-col md:space-y-0 space-y-10 md:space-x-10 pt-6 -top-36`} variants={staggerContainer} initial="hidden" whileInView="show">
+    <motion.section className={`relative ${styles.flexBetween} md:flex-row flex-col md:space-y-0 space-y-10 md:space-x-10 pt-6 -top-36`} variants={slideIn("right", "tween", 0.2, 1.5)} initial="hidden" whileInView="show">
         {RecentProjects.map((info) => (
             <ProjectCard category="Recents" title={info.title} description={info.description} imgURL={info.imgURL} icon={<AiFillGithub color='white'/>} link = {info.link} demoLink={info.demoLink} key={info.title}/>
         ))}

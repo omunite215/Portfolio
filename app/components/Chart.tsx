ef9c4c0d0c8140539e14ed26a94354e1b31316d5
omunite215 <omunite21@gmail.com>
2024-06-03 23:52:16 +0530

    Optimized Code

--- app/components/Chart.tsx
+++ app/components/Chart.tsx
@@ -1,31 +1,31 @@
 import Image from "next/image";
 
 const Chart = () => {
-  return (
-    <div
-      className="py-4 md:px-0 px-4 transition ease-in-out delay-250 hover:-translate-y-0.5 hover:scale-110"
-      id="skills"
-    >
-      <Image
-        src="/chart.svg"
-        quality={90}
-        height={1000}
-        width={1000}
-        priority={true}
-        alt="FlowChart"
-        className="object-fill sm:block hidden"
-      />
-      <Image
-        src="/chartMobile.svg"
-        quality={90}
-        height={1000}
-        width={1000}
-        priority={true}
-        alt="FlowChart"
-        className="object-fill sm:hidden block"
-      />
-    </div>
-  );
+	return (
+		<div
+			className="py-4 md:px-0 px-4 transition ease-in-out delay-250 hover:-translate-y-0.5 hover:scale-110"
+			id="skills"
+		>
+			<Image
+				src="/chart.svg"
+				quality={90}
+				height={1000}
+				width={1000}
+				priority={true}
+				alt="FlowChart"
+				className="object-fill sm:block hidden"
+			/>
+			<Image
+				src="/chartMobile.svg"
+				quality={90}
+				height={1000}
+				width={1000}
+				priority={true}
+				alt="FlowChart"
+				className="object-fill sm:hidden block"
+			/>
+		</div>
+	);
 };
 
 export default Chart;
--- types/index.ts
+++ types/index.ts
@@ -1,38 +1,38 @@
 export type CertificateCardProps = {
-    title: string;
-    description: string;
-    imageURL: string;
-    grade: string;
-    gradeColor: string;
-  }
-  
-  export type Tags = {
-    name: string;
-    color: string;
-  }
-  export type ProjectCardProps = {
-    title: string;
-    description: string;
-    imgURL: string;
-    icon: React.ReactNode;
-    category: string;
-    link: string;
-    demoLink: string;
-    tags: {
-      name: string;
-      color: string;
-    }[];
-  }
-  
-  export type SkillCardProps = {
-    id: number;
-    title: string;
-    content: React.ReactNode;
-  }
-  
-  export type SocialMediaCardProps = {
-    title: string;
-    description: string;
-    icon: React.ReactNode;
-    link: string;
-  }
+	title: string;
+	description: string;
+	imageURL: string;
+	grade: string;
+	gradeColor: string;
+};
+
+export type Tags = {
+	name: string;
+	color: string;
+};
+export type ProjectCardProps = {
+	title: string;
+	description: string;
+	imgURL: string;
+	icon: React.ReactNode;
+	category: string;
+	link: string;
+	demoLink: string;
+	tags: {
+		name: string;
+		color: string;
+	}[];
+};
+
+export type SkillCardProps = {
+	id: number;
+	title: string;
+	content: React.ReactNode;
+};
+
+export type SocialMediaCardProps = {
+	title: string;
+	description: string;
+	icon: React.ReactNode;
+	link: string;
+};
